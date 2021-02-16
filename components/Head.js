d8ae771d03639c7e7b8cbd01e5f380dcea7e585d
Yashsankpal <53860409+Yashsankpal@users.noreply.github.com>
2021-02-16 18:08:00 +0530

    completed

---
+++ components/Head.js
@@ -0,0 +1,10 @@
+import {Head as head} from 'next/head'
+
+export default function Head() {
+  return (
+    <head>
+        <title>Demo App</title>
+        <link rel="icon" href="/favicon.ico" />
+    </head>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,57 @@
-import Head from 'next/head'
+import { useState , useEffect} from 'react';
+import Head from '../components/head'
+import fetch from 'isomorphic-fetch'
+import axios from 'axios'
 import styles from '../styles/Home.module.css'
+import moment from 'moment'
 
 export default function Home() {
+
+  const [File,setFile] = useState(null);
+  const [isSelected , setisSelected] = useState(true)
+  const onSubmit = async() =>{
+    if(File){
+      if(!(File.size > 1024000)){
+        let formData = new FormData()
+        formData.append('File',File)
+        const res = await fetch('api/fileUpload',{
+          method:'POST',
+          headers:{
+            'auth-token':`${moment().format('DD-MM-YYYY')}${File.name.slice(0,3)}`
+          },
+          body:formData
+        })
+        console.log(res);
+      }
+    }
+  }
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Validate File Content in NextJs {':)'}
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
+      <div className={styles.grid}>
+        <input type='file' onChange={(e)=>setFile(e.target.files[0])} accept='image/*' className={styles.card}/>
+      </div>
+          {isSelected}
+      <input type='button' onClick={onSubmit} />
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+          Powered by Yash 
       </footer>
     </div>
   )
 }
+
+
+/**
+      if(!File.name.match(/\.(jpg|png)$/)){
+        console.log('Please select valid image.');
+        setisSelected(isSelected)
+        console.log(File);
+        return false;
+      }
+      if(File.size > 1024000){
+        console.log('File size too large!');
+ */
